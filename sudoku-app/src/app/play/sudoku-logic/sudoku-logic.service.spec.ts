89e03cfa7abaf987155d4990e2c7edc7893a5c83
crismora80 <62183514+crismora80@users.noreply.github.com>
2023-12-26 02:36:21 +0200

    implemented and tested method for checking if table is correct

--- sudoku-app/src/app/play/sudoku-logic/sudoku-logic.service.spec.ts
+++ sudoku-app/src/app/play/sudoku-logic/sudoku-logic.service.spec.ts
@@ -27,4 +27,109 @@ describe('SudokuLogicService', () => {
         }
         expect(service.checkIfTableIsFull(table)).toBeTruthy();
     });
+
+    it('#checkIfTableIsCorrect should return true if the table is empty', () => {
+        const table = service.generateEmptyTable();
+        expect(service.checkIfTableIsCorrect(table)).toBeTruthy();
+    });
+
+    it('#checkIfTableIsCorrect should return true if the table has one value', () => {
+        const table = service.generateEmptyTable();
+        table[8][8].value = "1";
+        expect(service.checkIfTableIsCorrect(table)).toBeTruthy();
+    });
+
+    it('#checkIfTableIsCorrect should return true if the table is completed correctly', () => {
+        const table = [
+            [{ value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "6", isFixed: false }, { value: "7", isFixed: false }, { value: "8", isFixed: false }, { value: "9", isFixed: false }, { value: "1", isFixed: false }, { value: "2", isFixed: false },],
+            [{ value: "6", isFixed: false }, { value: "7", isFixed: false }, { value: "2", isFixed: false }, { value: "1", isFixed: false }, { value: "9", isFixed: false }, { value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "8", isFixed: false },],
+            [{ value: "1", isFixed: false }, { value: "9", isFixed: false }, { value: "8", isFixed: false }, { value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "2", isFixed: false }, { value: "5", isFixed: false }, { value: "6", isFixed: false }, { value: "7", isFixed: false },],
+            [{ value: "8", isFixed: false }, { value: "5", isFixed: false }, { value: "9", isFixed: false }, { value: "7", isFixed: false }, { value: "6", isFixed: false }, { value: "1", isFixed: false }, { value: "4", isFixed: false }, { value: "2", isFixed: false }, { value: "3", isFixed: false },],
+            [{ value: "4", isFixed: false }, { value: "2", isFixed: false }, { value: "6", isFixed: false }, { value: "8", isFixed: false }, { value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "7", isFixed: false }, { value: "9", isFixed: false }, { value: "1", isFixed: false },],
+            [{ value: "7", isFixed: false }, { value: "1", isFixed: false }, { value: "3", isFixed: false }, { value: "9", isFixed: false }, { value: "2", isFixed: false }, { value: "4", isFixed: false }, { value: "8", isFixed: false }, { value: "5", isFixed: false }, { value: "6", isFixed: false },],
+            [{ value: "9", isFixed: false }, { value: "6", isFixed: false }, { value: "1", isFixed: false }, { value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "7", isFixed: false }, { value: "2", isFixed: false }, { value: "8", isFixed: false }, { value: "4", isFixed: false },],
+            [{ value: "2", isFixed: false }, { value: "8", isFixed: false }, { value: "7", isFixed: false }, { value: "4", isFixed: false }, { value: "1", isFixed: false }, { value: "9", isFixed: false }, { value: "6", isFixed: false }, { value: "3", isFixed: false }, { value: "5", isFixed: false },],
+            [{ value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "5", isFixed: false }, { value: "2", isFixed: false }, { value: "8", isFixed: false }, { value: "6", isFixed: false }, { value: "1", isFixed: false }, { value: "7", isFixed: false }, { value: "9", isFixed: false },]
+        ];
+        expect(service.checkIfTableIsCorrect(table)).toBeTruthy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table is complete and has a repeating value', () => {
+        // changed value [5][3] to 1
+        const table = [
+            [{ value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "6", isFixed: false }, { value: "7", isFixed: false }, { value: "8", isFixed: false }, { value: "9", isFixed: false }, { value: "1", isFixed: false }, { value: "2", isFixed: false },],
+            [{ value: "6", isFixed: false }, { value: "7", isFixed: false }, { value: "2", isFixed: false }, { value: "1", isFixed: false }, { value: "9", isFixed: false }, { value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "8", isFixed: false },],
+            [{ value: "1", isFixed: false }, { value: "9", isFixed: false }, { value: "8", isFixed: false }, { value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "2", isFixed: false }, { value: "5", isFixed: false }, { value: "6", isFixed: false }, { value: "7", isFixed: false },],
+            [{ value: "8", isFixed: false }, { value: "5", isFixed: false }, { value: "9", isFixed: false }, { value: "7", isFixed: false }, { value: "6", isFixed: false }, { value: "1", isFixed: false }, { value: "4", isFixed: false }, { value: "2", isFixed: false }, { value: "3", isFixed: false },],
+            [{ value: "4", isFixed: false }, { value: "2", isFixed: false }, { value: "6", isFixed: false }, { value: "8", isFixed: false }, { value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "7", isFixed: false }, { value: "9", isFixed: false }, { value: "1", isFixed: false },],
+            [{ value: "7", isFixed: false }, { value: "1", isFixed: false }, { value: "3", isFixed: false }, { value: "1", isFixed: false }, { value: "2", isFixed: false }, { value: "4", isFixed: false }, { value: "8", isFixed: false }, { value: "5", isFixed: false }, { value: "6", isFixed: false },],
+            [{ value: "9", isFixed: false }, { value: "6", isFixed: false }, { value: "1", isFixed: false }, { value: "5", isFixed: false }, { value: "3", isFixed: false }, { value: "7", isFixed: false }, { value: "2", isFixed: false }, { value: "8", isFixed: false }, { value: "4", isFixed: false },],
+            [{ value: "2", isFixed: false }, { value: "8", isFixed: false }, { value: "7", isFixed: false }, { value: "4", isFixed: false }, { value: "1", isFixed: false }, { value: "9", isFixed: false }, { value: "6", isFixed: false }, { value: "3", isFixed: false }, { value: "5", isFixed: false },],
+            [{ value: "3", isFixed: false }, { value: "4", isFixed: false }, { value: "5", isFixed: false }, { value: "2", isFixed: false }, { value: "8", isFixed: false }, { value: "6", isFixed: false }, { value: "1", isFixed: false }, { value: "7", isFixed: false }, { value: "9", isFixed: false },]
+        ];
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table has a repeating value on a row', () => {
+        const table = service.generateEmptyTable();
+        table[2][3].value = "1";
+        table[2][8].value = "1";
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table has a full row and a repeating value on that row', () => {
+        const table = service.generateEmptyTable();
+        table[2][0].value = "2";
+        table[2][1].value = "8";
+        table[2][2].value = "3";
+        table[2][3].value = "1";
+        table[2][4].value = "9";
+        table[2][5].value = "4";
+        table[2][6].value = "7";
+        table[2][7].value = "6";
+        table[2][8].value = "9";
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table has a repeating value on a column', () => {
+        const table = service.generateEmptyTable();
+        table[3][2].value = "1";
+        table[8][2].value = "1";
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table has a full column and a repeating value on that column', () => {
+        const table = service.generateEmptyTable();
+        table[0][2].value = "2";
+        table[1][2].value = "8";
+        table[2][2].value = "3";
+        table[3][2].value = "1";
+        table[4][2].value = "5";
+        table[5][2].value = "8";
+        table[6][2].value = "7";
+        table[7][2].value = "6";
+        table[8][2].value = "9";
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table has a repeating value on a 3x3 aquare', () => {
+        const table = service.generateEmptyTable();
+        table[1][1].value = "1";
+        table[2][0].value = "1";
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
+
+    it('#checkIfTableIsCorrect should return false if the table has a full 3x3 square and a repeating value on that square', () => {
+        const table = service.generateEmptyTable();
+        table[0][0].value = "2";
+        table[0][1].value = "4";
+        table[0][2].value = "3";
+        table[1][0].value = "1";
+        table[1][1].value = "5";
+        table[1][2].value = "8";
+        table[2][0].value = "7";
+        table[2][1].value = "4";
+        table[2][2].value = "9";
+        expect(service.checkIfTableIsCorrect(table)).toBeFalsy();
+    });
 });
--- sudoku-app/src/app/play/sudoku-logic/sudoku-logic.service.ts
+++ sudoku-app/src/app/play/sudoku-logic/sudoku-logic.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 
-import { SudokuTable } from '../table/table.mode';
+import { SudokuTable, TileValue } from '../table/table.mode';
 
 @Injectable()
 export class SudokuLogicService {
@@ -25,5 +25,47 @@ export class SudokuLogicService {
     checkIfTableIsFull(sudokuTable: SudokuTable): boolean {
         const emptyTile = sudokuTable.find(row => row.find(cell => !cell.value));
         return !emptyTile;
+    }
+
+    checkIfTableIsCorrect(sudokuTable: SudokuTable): boolean {
+        // check if rows have duplicates
+        for (let i = 0; i < 9; i++) {
+            const definedValuesInRow = sudokuTable[i].filter(cell => !!cell.value).map(cell => cell.value);
+            if ((new Set(definedValuesInRow)).size !== definedValuesInRow.length) {
+                return false;
+            }
+        }
+
+        // check if columns have duplicates 
+        for (let j = 0; j < 9; j++) {
+            const definedValuesInColumn = [];
+            for (let i = 0; i < 9; i++) {
+                if (!!sudokuTable[i][j].value) {
+                    definedValuesInColumn.push(sudokuTable[i][j].value)
+                }
+            }
+            if ((new Set(definedValuesInColumn)).size !== definedValuesInColumn.length) {
+                return false;
+            }
+        }
+
+        // check if 3x3 squares have duplicate elements
+        for (let k = 0; k < 3; k++) {
+            for (let l = 0; l < 3; l++) {
+                const definedValuesInSquare = [];
+                for (let i = k * 3; i < (k + 1) * 3; i++) {
+                    for (let j = l * 3; j < (l + 1) * 3; j++) {
+                        if (!!sudokuTable[i][j].value) {
+                            definedValuesInSquare.push(sudokuTable[i][j].value)
+                        }
+                    }
+                }
+                if ((new Set(definedValuesInSquare)).size !== definedValuesInSquare.length) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
