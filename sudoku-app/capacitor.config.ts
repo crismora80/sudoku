35118dcb5fc8e758a063014cc3b5fc4326770b7f
crismora80 <62183514+crismora80@users.noreply.github.com>
2024-02-04 16:39:08 +0200

    updated app name

--- sudoku-app/capacitor.config.ts
+++ sudoku-app/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sudoku-app',
+  appName: 'Sudoku',
   webDir: 'www',
   server: {
     androidScheme: 'https'
