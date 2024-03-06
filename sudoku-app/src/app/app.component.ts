9f73fabedfb3a2feae3c86f1eb190f9488c5f165
crismora80 <62183514+crismora80@users.noreply.github.com>
2024-03-07 01:22:52 +0200

    implemented 'Exit app' through hardware button

--- sudoku-app/src/app/app.component.ts
+++ sudoku-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ScreenOrientation, OrientationType } from '@capawesome/capacitor-screen-orientation';
+import { IonRouterOutlet, Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,14 @@ import { ScreenOrientation, OrientationType } from '@capawesome/capacitor-screen
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
+  @ViewChild(IonRouterOutlet) routerOutlet!: IonRouterOutlet;
+
+  constructor(private platform: Platform) {
     ScreenOrientation.lock({ type: OrientationType.PORTRAIT });
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet?.canGoBack()) {
+        App.exitApp();
+      }
+    });
   }
 }
